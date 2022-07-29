5aefe256624ce98b47f1f7412256538e1b8d1b9c
Darling Rodriguez <darlingrodriguez@Darlings-MacBook-Pro.local>
2022-07-29 00:46:54 -0400

    fixed dates

--- scripts/basic.js
+++ scripts/basic.js
@@ -1,6 +1,3 @@
-
-//   Code for choosing date
-
 var currentDateTime = new Date();
 var year = currentDateTime.getFullYear();
 var month = (currentDateTime.getMonth() + 1);
@@ -19,34 +16,46 @@ var AvalD = document.querySelector("#dates");
 AvalD.setAttribute("min", dateTomorrow);
 
 
+
 // Code for opening the popup
-
-
-
 const cellDivs = document.querySelectorAll('.grid-cell');
 const bgO = document.querySelector('.bg-cont');
 const popC = document.querySelector('.close');
 
 
-for (const cellD of cellDivs) {
+let spots = { 'm1': '5', 'm2': '5', 'm3': '5', 'm4': '5', 'm5': '5', 'm6': '5'};
 
+// Put the object into storage
+window.localStorage.setItem('spots', JSON.stringify(spots));
+
+// Retrieve the object from storage
+let newObject = window.localStorage.getItem("spots");
+newObject = JSON.parse(newObject);
+console.log(newObject['m1']);
+
+
+for (const cellD of cellDivs) {
     //event for when a cell is clicked
     cellD.addEventListener('click', function(event){
 
         const classList = event.target.classList;
 
+        // //this gets the selected date
+        // const input = document.querySelector('#dates')
+        // input.onchange = (e) => {
+        // console.log(e.target.value)
+        // }
+    
         if (classList[1] != 'void') {
 
             bgO.style.display = 'flex';
         }
-
     })
+    
 
     popC.addEventListener('click', function(){
 
         bgO.style.display = 'none';
     
         })
-
-
   }
