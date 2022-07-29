72864568563757d8885a4532619ad094f6e95e70
Darling Rodriguez <darlingrodriguez@Darlings-MacBook-Pro.local>
2022-07-28 23:03:18 -0400

    added functionality

--- scripts/basic.js
+++ scripts/basic.js
@@ -1,3 +1,28 @@
+
+//   Code for choosing date
+
+var currentDateTime = new Date();
+var year = currentDateTime.getFullYear();
+var month = (currentDateTime.getMonth() + 1);
+var date = (currentDateTime.getDate() + 1);
+
+if(date < 10) {
+  date = '0' + date;
+}
+if(month < 10) {
+  month = '0' + month;
+}
+
+var dateTomorrow = year + "-" + month + "-" + date;
+var AvalD = document.querySelector("#dates");
+
+AvalD.setAttribute("min", dateTomorrow);
+
+
+// Code for opening the popup
+
+
+
 const cellDivs = document.querySelectorAll('.grid-cell');
 const bgO = document.querySelector('.bg-cont');
 const popC = document.querySelector('.close');
@@ -8,11 +33,12 @@ for (const cellD of cellDivs) {
     //event for when a cell is clicked
     cellD.addEventListener('click', function(event){
 
-    //   const classList = event.target.classList;
-      
-    //   console.log(classList);
+        const classList = event.target.classList;
 
-    bgO.style.display = 'flex';
+        if (classList[1] != 'void') {
+
+            bgO.style.display = 'flex';
+        }
 
     })
 
