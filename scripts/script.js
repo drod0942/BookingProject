8b13882f3f913c2eda1a2e00a80ab7aa9122f939
Darling Rodriguez <darlingrodriguez@Darlings-MacBook-Pro.local>
2022-08-01 16:48:29 -0400

    added effects

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,30 @@
-let test;
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in');
+
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -130px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver
+(function(
+entries,
+appearOnScroll
+){
+    entries.forEach(entry => {
+        if(!entry.isIntersecting){
+            return;
+        }else{
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+},appearOptions);
+
+faders.forEach(fader =>{
+    appearOnScroll.observe(fader);
+})
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+})
